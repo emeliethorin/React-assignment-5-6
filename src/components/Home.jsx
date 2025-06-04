ec1dd325c85537421f40ee6fab92090eb8c8306d
emeliethorin <emeliethorin10@gmail.com>
2025-06-04 20:51:58 +0200

    changed to link component to handle routes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+ 
 function Home() {
     return (
-        <div>
-            <h1>Welcome to the <br/> Dog Guessing Game!</h1>
-            <p><a href="/Registration">Register</a> or 
-            <a href="/Login"> Login</a> to start playing.</p>
-        </div>
+<div>
+    <h1>Welcome to the <br /> Dog Guessing Game!</h1>
+    <p>
+    <Link to="/registration">Register</Link> or
+    <Link to="/login"> Login</Link> to start playing.
+    </p>
+</div>
     );
 }
-
+ 
 export default Home;
